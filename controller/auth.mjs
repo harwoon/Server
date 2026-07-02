43558e34d3fbe587fabf122943f7e92f0ba95087
한혜원 <dorothy8380@gmail.com>
2026-07-02 15:24:37 +0900

    로그인 API 완료

--- controller/auth.mjs
+++ controller/auth.mjs
@@ -26,7 +26,20 @@ export async function signup(req, res) {
 
 // 로그인
 export async function login(req, res) {
-
+    const { userid, password } = req.body
+    const user = await authRepository.findByUseid(userid)
+    if (user) {
+        const isValidPassword = await bcrypt.compare(password, user.password)
+        if (!isValidPassword) {
+            return res.status(401).json({ message: "아이디 또는 비밀번호 확인" })
+        }
+    } else {
+        if (!isValidPassword) {
+            return res.status(401).json({ message: "아이디 또는 비밀번호 확인" })
+        }
+    }
+    const token = await createJwtToken(user.id)
+    res.status(200).json({ token, user })
 }
 
 // 로그인 유지
