e275937a73309a497719fb4de67ee5a389cdc6b3
한혜원 <dorothy8380@gmail.com>
2026-07-03 11:28:30 +0900

    전체 포스트 가져오기 API 완료

--- controller/posts.mjs
+++ controller/posts.mjs
@@ -9,3 +9,10 @@ export async function createPost(req, res) {
     res.status(201).json(post)
     
 }
+
+// 모든 포스트를 가져오는 함수
+export async function getPosts(req,res){
+    const userid = req.query.userid
+    const data = await (userid ? postRepository.getAllByUserid(userid) : postRepository.getAll())
+    res.status(200).json(data)
+}
--- router/posts.mjs
+++ router/posts.mjs
@@ -5,7 +5,9 @@ import * as postController from "../controller/posts.mjs"
 const router = express.Router()
 
 // 전체 포스트 가져오기
-
+// http://127.0.0.1:8080/post (GET)
+// http://127.0.0.1:8080/post?userid=apple (GET)
+router.get("/",isAuth,postController.getPosts)
 
 // 글번호에 대한 포스트 가져오기
 
