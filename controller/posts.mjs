c5accd9dacdee7c8420ca0259e3b4545650de271
한혜원 <dorothy8380@gmail.com>
2026-07-03 10:28:12 +0900

    포스트 쓰기 API 완료

---
+++ controller/posts.mjs
@@ -0,0 +1,11 @@
+import express from "express"
+import * as postRepository from "../data/posts.mjs"
+
+
+// 포스트를 작성하는 함수
+export async function createPost(req, res) {
+    const { text } = req.body
+    const post = await postRepository.create(text, req.id)
+    res.status(201).json(post)
+    
+}
--- router/posts.mjs
+++ router/posts.mjs
@@ -1,10 +1,17 @@
 import express from "express"
+import { isAuth } from "../middleware/auth.mjs"
+import * as postController from "../controller/posts.mjs"
 
 const router = express.Router()
 
 // 전체 포스트 가져오기
 
+
 // 글번호에 대한 포스트 가져오기
+
+// 포스트 쓰기
+// http://127.0.0.1:8080/post
+router.post("/", isAuth, postController.createPost) // 로그인한 사용자만 작성 가능하도록
 
 // 포스트 수정하기
 
